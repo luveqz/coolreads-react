10b9f180c5dca29dbdfc10adcec49742c702a629
Luis Velásquez <luvejo.dev@gmail.com>
2022-06-18 18:53:13 -0500

    style: add custom Prettier rules

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   plugins: [require('prettier-plugin-tailwindcss')],
-  tailwindConfig: "./tailwind.config.js",
-};
+  tailwindConfig: './tailwind.config.js',
+
+  semi: false,
+  arrowParens: 'always',
+  singleQuote: true,
+  endOfLine: 'auto',
+  printWidth: 80,
+  trailingComma: 'all',
+  htmlWhitespaceSensitivity: 'strict',
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,23 +2,23 @@
 
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./layouts/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './layouts/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
 
   theme: {
     colors: {
       gray: {
-        100: "#B9B6BD",
-        200: "#908A8F",
-        300: "#817E8C",
-        400: "#38363E",
-        500: "#24222C",
-        600: "#1B1923",
+        100: '#B9B6BD',
+        200: '#908A8F',
+        300: '#817E8C',
+        400: '#38363E',
+        500: '#24222C',
+        600: '#1B1923',
       },
-      white: "#FFFFFF",
-      yellow: "#F8BA1A",
+      white: '#FFFFFF',
+      yellow: '#F8BA1A',
     },
   },
-};
+}
