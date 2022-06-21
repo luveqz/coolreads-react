bb7f11c9f2a189f9221fd4cd63d1779fa61d9093
Luis Velásquez <luvejo.dev@gmail.com>
2022-06-21 12:29:02 -0500

    feat: add ProfileInfo component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
+import ProfileInfo from '@/components/ProfileInfo'
 import type { NextPage } from 'next'
 
 const Profile: NextPage = () => {
   return (
-    <div className="mx-auto max-w-content-container px-5">
-      <main>Entries</main>
-      <aside>Secondary</aside>
+    <div className="mx-auto mt-9 max-w-content-container gap-x-10 px-5 md:flex">
+      <main className="md:w-8/12">
+        <ProfileInfo />
+      </main>
+
+      <aside className="md:w-4/12"></aside>
     </div>
   )
 }
