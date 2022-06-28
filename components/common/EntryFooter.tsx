04eabb1d21f5a8f96e7955fb5057273497af45be
Luis Velásquez <luvejo.dev@gmail.com>
2022-06-28 08:35:41 -0500

    feat: handle empty states on EntryFooter component

--- components/common/EntryFooter.tsx
+++ components/common/EntryFooter.tsx
@@ -11,14 +11,16 @@ const EntryFooter = ({ likes, comments }: Props) => {
     <section className="mt-3 flex justify-between">
       <div className="flex items-center gap-x-1">
         <LikeButton />
-        {likes}
+        {likes || ''}
       </div>
 
       <div className="flex items-center gap-x-4">
-        {comments.length && (
+        {comments.length ? (
           <a className="underline underline-offset-2" href="#">
             View {comments.length} comments
           </a>
+        ) : (
+          ''
         )}
         <div className="hidden md:block">
           <BaseButton variant="tertiary">Reply</BaseButton>
