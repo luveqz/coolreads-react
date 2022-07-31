e191fec21335456b5c56fb5cb4e2fcd7a52a8831
Luis Velásquez <luvejo.dev@gmail.com>
2022-07-31 15:14:32 -0500

    chore: update schema fields restrictions and replace resolver's mocked logic

--- graphql/resolvers.ts
+++ graphql/resolvers.ts
@@ -1,15 +1,13 @@
-import { User, QueryUserArgs } from '@/.output/graphql'
+import { PrismaClient } from '@prisma/client'
+import { QueryUserArgs } from '@/.output/graphql'
 
-function user(_ctx: {}, { username }: QueryUserArgs): User {
-  return {
-    id: '5',
-    firstName: 'Haylee',
-    lastName: 'Caulfield',
-    username: 'goldenfish',
-    bio: 'I could’ve told you my whole autobiography, but I didn’t felt like it.',
-    avatarUrl:
-      'https://firebasestorage.googleapis.com/v0/b/fictolab.appspot.com/o/coolreads%2Fhaylee.webp?alt=media',
-  }
+const prisma = new PrismaClient()
+
+async function user(_ctx: {}, { username }: QueryUserArgs) {
+  const user = await prisma.user.findFirst({
+    where: { username },
+  })
+  return user
 }
 
 export default {
