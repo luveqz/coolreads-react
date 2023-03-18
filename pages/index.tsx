f24ccc32b32d5994ccfffc3ff63a9dd04a73dacc
Luis Velásquez <luvejo.dev@gmail.com>
2023-03-17 23:17:45 -0500

    chore: set page title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,6 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
 import TheFooter from '@/components/TheFooter'
 import ActivityTabs from '@/components/ActivityTabs'
 import ProfileHeader from '@/components/ProfileHeader'
@@ -8,50 +11,55 @@ import LeaderBoard from '@/components/widgets/LeaderBoard'
 import FeaturedBook from '@/components/widgets/FeaturedBook'
 import { review, quote, list } from '@/database/fixtures/recent-activity'
 import { featuredBook } from '@/database/fixtures/widgets'
-import type { NextPage } from 'next'
 
 const Profile: NextPage = () => {
   return (
-    <div className="mx-auto mt-9 max-w-content-container grid-cols-main gap-x-10 px-5 md:grid">
-      <main className="pb-11">
-        <ProfileHeader />
-        <ActivityTabs />
+    <>
+      <Head>
+        <title>Haylee Caulfield | CoolReads</title>
+      </Head>
 
-        <section className="flex flex-col gap-y-4">
-          <ReviewEntry
-            user={review.user}
-            book={review.book}
-            review={review.review}
-            meta={review.meta}
-          />
-          <QuoteEntry
-            user={quote.user}
-            book={quote.book}
-            quote={quote.quote}
-            meta={quote.meta}
-          />
-          <ListEntry
-            user={list.user}
-            books={list.books}
-            name={list.name}
-            meta={list.meta}
-          />
-        </section>
-      </main>
+      <div className="mx-auto mt-9 max-w-content-container grid-cols-main gap-x-10 px-5 md:grid">
+        <main className="pb-11">
+          <ProfileHeader />
+          <ActivityTabs />
 
-      <div className="hidden md:block">
-        <aside className="flex flex-col gap-y-10">
-          <LeaderBoard />
-          <FeaturedBook
-            title="Recommended by Haylee"
-            subtitle="This week"
-            book={featuredBook}
-          />
-        </aside>
+          <section className="flex flex-col gap-y-4">
+            <ReviewEntry
+              user={review.user}
+              book={review.book}
+              review={review.review}
+              meta={review.meta}
+            />
+            <QuoteEntry
+              user={quote.user}
+              book={quote.book}
+              quote={quote.quote}
+              meta={quote.meta}
+            />
+            <ListEntry
+              user={list.user}
+              books={list.books}
+              name={list.name}
+              meta={list.meta}
+            />
+          </section>
+        </main>
 
-        <TheFooter />
+        <div className="hidden md:block">
+          <aside className="flex flex-col gap-y-10">
+            <LeaderBoard />
+            <FeaturedBook
+              title="Recommended by Haylee"
+              subtitle="This week"
+              book={featuredBook}
+            />
+          </aside>
+
+          <TheFooter />
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
